564a8cef2dc1ba1928fba11ba56c909e74b66a6c
Tomáš Mozdřeň <46648569+BeanGreen247@users.noreply.github.com>
2023-02-16 08:53:23 +0100

    Update setupArchLinux.sh

--- setupArchLinux.sh
+++ setupArchLinux.sh
@@ -31,6 +31,7 @@ function main {
     pass="test"
     username="test"
     gpudriver="amd"
+    zramsize=32768 #check Setting up zram
 
     #cannot automate, causes issues
     #echo $pass |sudo -S echo "[multilib]" >> /etc/pacman.conf
@@ -42,16 +43,16 @@ function main {
 
     echo "Scripts Prep..."
     mkdir /home/$username/scripts
-    echo "#!/bin/bash" > /home/$username/scripts/performance-mode.sh
-    echo "cat /sys/devices/system/cpu/cpu*/cpufreq/scaling_governor" >> /home/$username/scripts/performance-mode.sh
-    echo 'for file in /sys/devices/system/cpu/cpu*/cpufreq/scaling_governor; do echo "performance" > $file; \e[1;32m[DONE]\e[1;0m' >> /home/$username/scripts/performance-mode.sh
-    echo "cat /sys/devices/system/cpu/cpu*/cpufreq/scaling_governor" >> /home/$username/scripts/performance-mode.sh
+    echo "#!/bin/bash" | tee /home/$username/scripts/performance-mode.sh
+    echo "cat /sys/devices/system/cpu/cpu*/cpufreq/scaling_governor" | tee -a /home/$username/scripts/performance-mode.sh
+    echo 'for file in /sys/devices/system/cpu/cpu*/cpufreq/scaling_governor; do echo "performance" > $file; \e[1;32m[DONE]\e[1;0m' | tee -a /home/$username/scripts/performance-mode.sh
+    echo "cat /sys/devices/system/cpu/cpu*/cpufreq/scaling_governor" | tee -a /home/$username/scripts/performance-mode.sh
 
-    echo "#!/bin/bash" > /home/$username/scripts/execute-performance-mode-sh-as-root.sh
-    echo 'pass="test"' >> /home/$username/scripts/execute-performance-mode-sh-as-root.sh
-    echo 'sleep 3' >> /home/$username/scripts/execute-performance-mode-sh-as-root.sh
-    echo 'chmod +x performance-mode.sh' >> /home/$username/scripts/execute-performance-mode-sh-as-root.sh
-    echo 'echo $pass | sudo -S bash performance-mode.sh' >> /home/$username/scripts/execute-performance-mode-sh-as-root.sh
+    echo "#!/bin/bash" | tee /home/$username/scripts/execute-performance-mode-sh-as-root.sh
+    echo 'pass="'$pass'"' | tee -a /home/$username/scripts/execute-performance-mode-sh-as-root.sh
+    echo 'sleep 3' | tee -a /home/$username/scripts/execute-performance-mode-sh-as-root.sh
+    echo 'chmod +x performance-mode.sh' | tee -a /home/$username/scripts/execute-performance-mode-sh-as-root.sh
+    echo 'echo '$pass' | sudo -S bash performance-mode.sh' | tee -a /home/$username/scripts/execute-performance-mode-sh-as-root.sh
 
     echo -e '\e[1;31m[IMPORTANT MESSAGE, PLEASE READ]\e[1;0m\n'
     echo 'The following scripts were created, make sure to add them to startup items.'
@@ -64,12 +65,26 @@ function main {
     echo -e "\e[1;31m[Will resume in 30s...]\e[1;0m\n"
     sleep 30
     echo 'Installing GUI, apps and other packages...'
-    echo $pass | sudo -S pacman -S --noconfirm --needed htop neofetch xorg cpupower firefox pulseaudio libreoffice base-devel git vim vi cronie net-tools linux-headers asp plasma plasma-wayland-session kde-applications obs-studio jre-openjdk jre11-openjdk jre8-openjdk jdk-openjdk jdk11-openjdk jdk8-openjdk gcc glibc gamemode lib32-gamemode lib32-vkd3d vkd3d git-lfs tmux filezilla ksysguard fakeroot
+    echo $pass | sudo -S pacman -S --noconfirm --needed htop neofetch xorg cpupower firefox pulseaudio libreoffice base-devel git vim vi cronie net-tools linux-headers asp plasma plasma-wayland-session kde-applications obs-studio jre-openjdk jre11-openjdk jre8-openjdk jdk-openjdk jdk11-openjdk jdk8-openjdk gcc glibc gamemode lib32-gamemode lib32-vkd3d vkd3d git-lfs tmux filezilla ksysguard fakeroot zram-generator
     echo $pass | sudo -S systemctl enable cronie sddm
     echo $pass | sudo -S pacman -S --noconfirm --needed giflib lib32-giflib libpng lib32-libpng libldap lib32-libldap gnutls lib32-gnutls mpg123 lib32-mpg123 openal lib32-openal v4l-utils lib32-v4l-utils libpulse lib32-libpulse libgpg-error lib32-libgpg-error alsa-plugins lib32-alsa-plugins alsa-lib lib32-alsa-lib libjpeg-turbo lib32-libjpeg-turbo sqlite lib32-sqlite libxcomposite lib32-libxcomposite libxinerama lib32-libgcrypt libgcrypt lib32-libxinerama ncurses lib32-ncurses opencl-icd-loader lib32-opencl-icd-loader libxslt lib32-libxslt libva lib32-libva gtk3 lib32-gtk3 gst-plugins-base-libs lib32-gst-plugins-base-libs vulkan-icd-loader lib32-vulkan-icd-loader cups samba alsa alsa-utils alsa-tools gnutls libpng wine-mono lib32-libxml2 lib32-mpg123 lib32-lcms2 lib32-giflib lib32-libpng lib32-gnutls pygtk python2-dbus lib32-libpulse lib32-fontconfig lib32-libxcomposite lib32-libxrender  lib32-libxslt lib32-gnutls lib32-libxi lib32-libxrandr lib32-libxinerama lib32-libcups lib32-freetype2 lib32-libpng lib32-openal python-pyopencl lib32-v4l-utils lib32-libxcursor lib32-mpg123 lib32-sdl xf86-video-intel lib32-mesa-libgl nss-mdns
     echo $pass | sudo -S systemctl enable cpupower
     echo $pass | sudo -S systemctl start cpupower
     echo -e "Installing GUI, apps and other packages...\e[1;32m[DONE]\e[1;0m"
+
+    echo -e "\e[1;31m[Setting up zram...]\e[1;0m\n"
+    echo $pass | sudo -S echo '# This config file enables a /dev/zram0 device with the default settings:' | sudo tee /usr/lib/systemd/zram-generator.conf
+    echo $pass | sudo -S echo '# - size - same as available RAM or 32GB, whichever is less' | sudo tee -a /usr/lib/systemd/zram-generator.conf
+    echo $pass | sudo -S echo '#        - if your system/VM has 16GB RAM then change it from 32768 to 16384' | sudo tee -a /usr/lib/systemd/zram-generator.conf
+    echo $pass | sudo -S echo '# - compression - most likely lzo-rle' | sudo tee -a /usr/lib/systemd/zram-generator.conf
+    echo $pass | sudo -S echo '# To disable, uninstall zram-generator-defaults or create empty' | sudo tee -a /usr/lib/systemd/zram-generator.conf
+    echo $pass | sudo -S echo '# /etc/systemd/zram-generator.conf file.' | sudo tee -a /usr/lib/systemd/zram-generator.conf
+    echo $pass | sudo -S echo '[zram0]' | sudo tee -a /usr/lib/systemd/zram-generator.conf
+    echo $pass | sudo -S echo 'zram-size = min(ram, '$zramsize')' | sudo tee -a /usr/lib/systemd/zram-generator.conf
+    echo $pass | sudo -S systemctl daemon-reload
+    echo $pass | sudo -S systemctl start /dev/zram0
+    echo $pass | sudo -S zramctl
+    echo -e "Setting up zram...\e[1;32m[DONE]\e[1;0m"
 
     echo 'Installing GPU Drivers...'
     if [ "$gpudriver" == "amd" ]; then
